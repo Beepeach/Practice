dee040dbdd279d1bc715a6200fc18ccdbc338183
JOJUNHEE <eh7457303j@gmail.com>
2020-09-25 11:16:15 +0900

    Make project image insert
    Way to insert image

--- 9월 4주차/insertImage200923/insertImage200923/ViewController.swift
+++ 9월 4주차/insertImage200923/insertImage200923/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    // ?가 붙어있어서 ?? 로 없을때 nil을 반환하도록 했다.
+    let beepeach = UIImage(named: "Beepeach") ?? nil
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 이걸 추가해야 화면에 나타났다.
+        imageView.image = beepeach
+    
         // Do any additional setup after loading the view.
     }
-
 
 }
 
